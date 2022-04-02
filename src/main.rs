52eea89474d4f1d657bd271e0c037b916fa10d1c
matthew colson <matthew.p.colson@gmail.com>
2022-04-02 15:50:06 -0500

    Added commits to main.rs file.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+/*
+    Project:        guessing_game
+    File:           main.rs
+    Date Created:   04/02/2022
+    Author:         Matthew Colson
+    Purpose:        Simple guess the number game.
+*/
+
 use std::io;
 use std::cmp::Ordering;
 use rand::Rng;
@@ -5,18 +13,21 @@ use rand::Rng;
 fn main() {
     println!("<---  NUMBER GUESSING GAME!  --->");
 
+    // Generates a random number between 1 and 100
     let secret_number = rand::thread_rng().gen_range(1..101);
     let mut times_guessed: u32 = 0;
 
+    // Loop until user guesses correct number
     loop {
         println!("Please input your guess: ");
         let mut guess = String::new();
 
+        // Reads in user input
         io::stdin()
             .read_line(&mut guess)
             .expect("Failed to read line");
 
-        
+        // Parses user input string into a unsigned 32 bit int 
         let guess: u32 = match guess.trim().parse() {
             Ok(num) => num,
             Err(_) => {
@@ -28,6 +39,7 @@ fn main() {
         println!("You guessed: {}", guess);
         times_guessed += 1;
         
+        // Compares guess from random number and alerts user of result
         match guess.cmp(&secret_number) {
             Ordering::Less => println!("To small!"),
             Ordering::Greater => println!("To big!"),
